42f26157b57ba06641eeb78ab21d469af44e41b2
kimsappi <55164185+kimsappi@users.noreply.github.com>
2019-10-17 00:14:10 +0300

    First linked list function

---
+++ linkedlist.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	if (!content)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	else
+	{
+		new->content = malloc(content_size);
+		if (!(new->content))
+		{
+			free(new);
+			return (NULL);
+		}
+		new->content_size = content_size;
+	}
+	new->next = NULL;
+	return (new);
+}
