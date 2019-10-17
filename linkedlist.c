04beca5b77941ff10fc3434dbb4ad7d1cb43caa1
kimsappi <55164185+kimsappi@users.noreply.github.com>
2019-10-17 20:30:24 +0300

    Everything but ft_lstmap done, largely untested

--- linkedlist.c
+++ linkedlist.c
@@ -28,8 +28,66 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 			free(new);
 			return (NULL);
 		}
+		new->content = ft_memcpy(new->content, content, content_size);
 		new->content_size = content_size;
 	}
 	new->next = NULL;
 	return (new);
+}
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (alst)
+	{
+		if (*alst)
+		{
+			(*del)((void *)(*alst)->content, (*alst)->content_size);
+			free(*alst);
+			*alst = NULL;
+		}
+	}
+}
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*next;
+	t_list	*current;
+
+	if (alst)
+	{
+		current = *alst;
+		while (current)
+		{
+			next = current->next;
+			(*del)(current->content, current->content_size);
+			free(current);
+			current = next;
+		}
+		*alst = NULL;
+	}
+}
+
+void	ft_lstadd(t_list **alst, t_list *new)
+{
+	if (!new)
+		return ;
+	if (alst)
+	{
+		new->next = *alst;
+		*alst = new;
+	}
+}
+
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
+{
+	while (lst)
+	{
+		(*f)(lst);
+		lst = lst->next;
+	}
+}
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	
 }
