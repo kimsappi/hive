3ae94f7569d12c5d6cebe48366d755f7a59e8dd3
Kim Säppi <55164185+kimsappi@users.noreply.github.com>
2019-10-20 21:44:56 +0300

    Basic structure of get_next_line for single fd

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,7 +1,24 @@
 int		get_next_line(const int fd, char **line)
 {
-	static buff[BUFF_SIZE + 1] = ft_memset(;
+	static char	buff[BUFF_SIZE + 1];
+	ssize_t		read_bytes;
+	static char	*newline = NULL;
 	
 	if (fd < 0 || !line || !(*line))
 		return (-1);
-	
+	if (newline)
+		ft_strcat(*line, newline + 1);
+	if (ft_strchr(newline + 1, '\n'))
+		return (*line);
+	while (read_bytes = read(fd, buff, BUFF_SIZE))
+	{
+		if (read_bytes == -1)
+			return (-1);
+		buff[read_bytes] = 0;
+		if (newline = ft_strchr(buff, '\n'))
+			break;
+		ft_strcat(*line, buff);
+	}
+
+	return (*line);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,7 +1,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "libft.h"
-# include <fcntl.h>
 # define BUFF_SIZE 10
 
 int	get_next_line(const int fd, char **line);
