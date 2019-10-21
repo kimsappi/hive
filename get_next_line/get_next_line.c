9dd41422a48e343d10d529e716c2a39aaa4bcdc7
Kim Sappi <ksappi@student.hive.fi>
2019-10-21 16:31:10 +0300

    Can now read a little bit from files, empty lines ('\n') still confuse it

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,3 +1,7 @@
+#include "get_next_line.h"
+#include <fcntl.h> //vitsiin
+#include <stdio.h> //vitsiin
+
 int		get_next_line(const int fd, char **line)
 {
 	static char	buff[BUFF_SIZE + 1];
@@ -10,21 +14,43 @@ int		get_next_line(const int fd, char **line)
 	{
 		if (ft_strchr(newline + 1, '\n'))
 		{
-			ft_strncat(*line, newline + 1, (ft_strchr(newline + 1, '\n') - (newline + 1)) / sizeof(char));
-			return (*line);
+			ft_strncat(*line, newline + 1, (ft_strchr(newline + 1, '\n') - (newline)) / sizeof(char));
+			newline = ft_strchr(newline + 1, '\n');
+			return (1);
 		}
 		else
-			ft_strcat(*line, newline + 1);
+		{
+			ft_strcat(*line, newline + sizeof(char));
+			newline = NULL;
+		}
 	}
-	while (read_bytes = read(fd, buff, BUFF_SIZE))
+	while ((read_bytes = read(fd, buff, BUFF_SIZE)))
 	{
 		if (read_bytes == -1)
 			return (-1);
+		if (read_bytes < BUFF_SIZE) // not sure if this will work with stdin
+			return (0);
 		buff[read_bytes] = 0;
-		if (newline = ft_strchr(buff, '\n'))
+		if ((newline = ft_strchr(buff, '\n')))
+		{
+			ft_strncat(*line, buff, newline - buff);
 			break;
+		}
 		ft_strcat(*line, buff);
 	}
+	return (1);
+}
 
-	return (*line);
+int main(void) // vitsiin
+{
+	char *line = malloc(1024);
+	int file = open("get_next_line.h", O_RDONLY);
+	int i = 1;
+	while (i > 0)
+	{
+		i = get_next_line(file, &line);
+		printf("%d: %s\n", i, line);
+		ft_strclr(line);
+	}
+	return (0);
 }
---
+++ get_next_line/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gcc get_next_line.c -Llibft -lft -Ilibft
+./a.out
