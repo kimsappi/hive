a00b6f7f81a44751d9eedf50b52a4700d7d876a2
Kim Sappi <ksappi@student.hive.fi>
2019-11-01 13:50:55 +0200

    ft_ceil_sqrt now properly handles near MAX_UINT values

--- fillit/libft/ft_ceil_sqrt.c
+++ fillit/libft/ft_ceil_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: ksappi <ksappi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 14:09:57 by ksappi            #+#    #+#             */
-/*   Updated: 2019/11/01 10:38:41 by ksappi           ###   ########.fr       */
+/*   Updated: 2019/11/01 13:13:12 by ksappi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,14 @@ int	ft_ceil_sqrt(unsigned int n)
 {
 	unsigned int	sqrt;
 
+	if (n > 4294836225)
+	return (65536);
 	sqrt = 0;
 	while (sqrt * sqrt < n)
 	{
-		if (sqrt > 65535)
+		if (sqrt > 65537)
 			return (-1);
 		++sqrt;
 	}
-	return (sqrt);
+	return ((int)sqrt);
 }
