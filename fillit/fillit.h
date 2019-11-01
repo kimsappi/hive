f3c44e02b8e5d33ea1311de8ab9779173769c55b
Kim Sappi <ksappi@student.hive.fi>
2019-11-01 10:24:24 +0200

    fillit structs

---
+++ fillit/fillit.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksappi <ksappi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/01 10:15:26 by ksappi            #+#    #+#             */
+/*   Updated: 2019/11/01 10:24:17 by ksappi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+# include "libft.h"
+
+typedef struct	s_piece
+{
+	char	letter; //letter that must be placed
+	char	x[3]; //x coordinate of each square relative to the first square read
+	char	y[3]; //same but y, same order
+	char	height; // total height of piece relative to first square read?
+					// #### = height 0?
+	char	width; // total width of piece relative to first square read
+					// #### = width 3?
+}				t_piece;
+
+typedef struct	s_board
+{
+	char	**board;
+	char	size;
+}				t_board;
+
+int				fillit_solver(t_board *board, t_list *piece);
+int				ft_ceil_sqrt(unsigned int n);
+
+#endif
