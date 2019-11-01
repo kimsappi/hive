3edb4d8c6224088831ec83e6be51e0045640fcb1
Kim Sappi <ksappi@student.hive.fi>
2019-11-01 14:37:36 +0200

    Beginnings of fillit file reading

--- fillit/main.c
+++ fillit/main.c
@@ -6,20 +6,41 @@
 /*   By: ksappi <ksappi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 15:45:48 by ksappi            #+#    #+#             */
-/*   Updated: 2019/10/31 15:53:48 by ksappi           ###   ########.fr       */
+/*   Updated: 2019/11/01 14:37:27 by ksappi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <fillit.h>
 
-int	main(int argc, char **argv)
+static char	fi_read_file(int fd)
 {
-	if (argc == 2)
+	t_list	*pieces;
+	char	rows;
+	char	row;
+
+	pieces = NULL;
+	rows = 0;
+	while (++rows < 5)
 	{
+		row = get_next_line(fd, &line);
 		
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	fd;
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			return (1);
+		else
+			fi_read_file(fd);
+	}
 	else
-		ft_putendl("usage: fillit [file]");
+		ft_putendl("usage: fillit file_name");
 	return (0); //return 1 if usage? Probably not but not sure
 }
