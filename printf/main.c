1c300aa3d9fe26c7b7fd87903a1354d28cbc7ecb
Kim Sappi <ksappi@student.hive.fi>
2019-11-12 13:47:48 +0200

    Cleaned up extra print in base conversion prints

--- printf/main.c
+++ printf/main.c
@@ -4,6 +4,6 @@ int main() {
 ft_printf("##########asd\n%s\n%c\n", "123", 'X');
 ft_printf("%-05da\n", 123);
 ft_printf("next line should be hex:\n");
-ft_printf("%x\n", 5454553);
+ft_printf("%o\n", 200);
 return 0;
 }
